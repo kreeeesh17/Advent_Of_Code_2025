7c6786fe2e0b6c13953140d35ecc112b5e996895
unknown <kreeshmodi.codeforces@gmail.com>
2025-12-11 15:11:36 +0530

    day 1 solved

---
+++ AdventOfCodeDay1P2.cpp
@@ -0,0 +1,188 @@
+/**************************************
+01/12
+Author - kreeeesh_17
+Created : 10:38:49
+**************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ll long long int
+#define inf 1000000000
+#define endl '\n'
+#define g(i, m, n) for (ll i = m; i < n; i++)
+#define vint vector<ll>
+#define ia(a, n) \
+    vint a(n);   \
+    f(i, n) cin >> a[i];
+#define asc(a) (a).begin(), (a).end()
+#define desc(a) (a).rbegin(), (a).rend()
+#define print(a)          \
+    for (auto x : (a))    \
+        cout << x << ' '; \
+    cout << endl;
+#define pb(x) push_back(x);
+#define pii pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define debarr(a, n)            \
+    cout << #a << " : ";        \
+    for (int i = 0; i < n; i++) \
+        cout << a[i] << " ";    \
+    cout << endl;
+#define debmat(mat, row, col)         \
+    cout << #mat << " :\n";           \
+    for (int i = 0; i < row; i++)     \
+    {                                 \
+        for (int j = 0; j < col; j++) \
+            cout << mat[i][j] << " "; \
+        cout << endl;                 \
+    }
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS)
+template <class S, class T>
+ostream &operator<<(ostream &os, const unordered_map<S, T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class S, class T>
+ostream &operator<<(ostream &os, const pair<S, T> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <class T>
+ostream &operator<<(ostream &os, const vector<T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class T>
+ostream &operator<<(ostream &os, const set<T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class T>
+ostream &operator<<(ostream &os, const multiset<T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class S, class T>
+ostream &operator<<(ostream &os, const map<S, T> &p)
+{
+    os << "[ ";
+    for (auto &it : p)
+        os << it << " ";
+    return os << "]";
+}
+template <class T>
+void dbs(string str, T t) { cout << str << " : " << t << "\n"; }
+template <class T, class... S>
+void dbs(string str, T t, S... s)
+{
+    int idx = str.find(',');
+    cout << str.substr(0, idx) << " : " << t << ",";
+    dbs(str.substr(idx + 1), s...);
+}
+template <class T>
+void prc(T a, T b)
+{
+    cout << "[";
+    for (T i = a; i != b; ++i)
+    {
+        if (i != a)
+            cout << ", ";
+        cout << *i;
+    }
+    cout << "]\n";
+}
+ll mod = 1e9 + 7;
+ll power(ll a, ll b)
+{
+    if (b == 0)
+        return 1;
+    if (b % 2)
+        return a * power(a, b - 1) % mod;
+    else
+    {
+        ll x = power(a, b / 2);
+        return (x * x) % mod;
+    }
+}
+ll inverse(ll x) { return power(x, mod - 2); }
+ll dx[] = {1, 0, -1, 0};
+ll dy[] = {0, 1, 0, -1};
+/********************************************************************************************************************************************************************************************/
+
+void solve()
+{
+    ll pos = 50, count = 0;
+    string s;
+    while (cin >> s)
+    {
+        if (s.empty())
+            continue;
+        char dir = s[0];
+        ll d = stoi(s.substr(1));
+        if (dir == 'R')
+        {
+            ll req = 100 - pos;
+            if (d >= req)
+            {
+                count++;
+                ll rem = d - req;
+                count += (rem / 100);
+            }
+            pos = (pos + d) % 100;
+        }
+        else
+        {
+            ll req;
+            if (pos == 0)
+            {
+                req = 100;
+            }
+            else
+            {
+                req = pos;
+            }
+            if (d >= req)
+            {
+                count++;
+                ll rem = d - req;
+                count += (rem / 100);
+            }
+            pos = pos - (d % 100);
+            if (pos < 0)
+            {
+                pos += 100;
+            }
+        }
+    }
+    cout << count << endl;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // #ifndef ONLINE_JUDGE
+    //   freopen("AdventOfCode25Day1_input.txt","r",stdin);
+    //   freopen("AdventOfCode25Day1_output.txt","w",stdout);
+    // #endif
+    ll t;
+    t = 1;
+    // cin >> t;
+    g(i, 1, t + 1)
+    {
+        // cout<<"Case #"<<i<<": ";
+        solve();
+    }
+    return 0;
+}
